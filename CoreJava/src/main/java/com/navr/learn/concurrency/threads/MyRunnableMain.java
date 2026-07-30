e0976060e985897ef44bba0dd23ab77309597e38
navrwork <149756645+navrwork@users.noreply.github.com>
2026-07-30 17:14:19 +0530

    refactor Core classes

--- CoreJava/src/main/java/com/navr/learn/concurrency/threads/MyRunnableMain.java
+++
@@ -1,9 +0,0 @@
-package com.navr.learn.concurrency.threads;
-
-public class MyRunnableMain {
-
-    public static void main(String[] args) {
-        Thread thread = new Thread(new MyRunnableTask());
-        thread.start();
-    }
-}
